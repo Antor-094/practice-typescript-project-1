187265ec46203b99d1271278b19023af30be756d
Antor-094 <mdantor556677889@gmail.com>
2023-11-19 07:28:27 +0600

    initial setup done

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import app from './app';
+import mongoose from 'mongoose';
+import config from './app/config/index';
+
+async function main() {
+  try {
+    await mongoose.connect(config.DATABASE_URL as string);
+    app.listen(config.PORT, () => {
+      console.log(`Example app listening on port ${config.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
